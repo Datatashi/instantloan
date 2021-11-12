d9604217a0ff5a03fae09010e338babd3393cfee
Datatashi <93118153+Datatashi@users.noreply.github.com>
2021-11-12 11:05:41 +0600

    Update Instant_loan.py

--- Instant_loan.py
+++ Instant_loan.py
@@ -6,9 +6,7 @@ from PIL import Image
 
 img = Image.open("./logoai.png")
 
-st.set_page_config(page_title="8o8 Instant Loan",
-                   page_icon=img,
-                   layout= "wide")
+
 st.sidebar.image(img, width= 200)
 
 model = pickle.load(open("model.sav", "rb"))
@@ -61,71 +59,3 @@ if result:
 
 
 
-
-
-
-
-
-
-
-
-# if Account_number == data["ACCTT_NO"]:
-#     st.write()
-#     st.write(data = {'Age': data["AGE"],'Credit_card': data["CREDIT_CARD"],'Current_Balance': data["CURR_BAL"]})
-#
-# def user_input_features():
-#     Age = st.sidebar.slider("What is your age?", 1,100,18)
-#     Credit_card = st.sidebar.selectbox('Do you have the Credit card?', (1,0))
-#     Saving_Balance_Amount = st.sidebar.number_input("What is the Balance Amount in your account?")
-#
-#     df = {'Age': Age,
-#             'Credit_card': Credit_card,
-#             'Saving_Balance_Amount': Saving_Balance_Amount}
-#     features = pd.DataFrame(df, index=[0])
-#     return features
-#
-# df = user_input_features()
-#
-# st.subheader('Account Holder Input parameters')
-# st.write(df)
-#
-# data = pd.read_csv("D:/AI/data_AI_IL.csv")
-
-# X = data.iloc[0:764,0:3].values
-# y = data.iloc[0:764,3].values
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
-#
-#
-# # model = LogisticRegression()
-# # model.fit(X_train, y_train)
-#
-# clf = RandomForestClassifier()
-# clf.fit(X, y)
-#
-#
-# #
-# # prediction = clf.predict(df)
-# # prediction_proba = clf.predict_proba(df)
-#
-# st.subheader('Parameters and the approval of corresponding Instant loan')
-# st.write(df.INSTANT_LOAN)
-#
-# st.subheader('Prediction')
-# st.write(df.INSTANT_LOAN[prediction])
-# #st.write(prediction)
-#
-# st.subheader('Prediction Probability')
-# st.write(prediction_proba)
-
-#
-
-# lr_prediction = model.predict(X_test)
-#
-# st.subheader('PREDICTION OF INSTANT LOAN')
-# st.write(df["INSTANT_LOAN"])
-#
-# st.subheader('Prediction')
-# st.write(df.INSTANT_LOAN[lr_prediction])
-#
-# st.write("Logistic Regression Accuracy:", metrics.accuracy_score(lr_prediction, y_test))
